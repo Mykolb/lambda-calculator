b75206b805e02f602518a40b45b7f33682ef680e
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-07-16 22:47:59 -0400

    components rendering, needs styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Logo from "./components/DisplayComponents/Logo";
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Display from './components/DisplayComponents/Display';
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
          <Operators />
